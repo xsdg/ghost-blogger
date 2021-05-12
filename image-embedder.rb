d2db5053d3c0db94779e955c5cca3b259df81226
Omari Stephens <xsdg@xsdg.org>
2021-05-12 19:46:16 +0000

    Correctly infers src and local dest for image downloading/embedding

--- image-embedder.rb
+++ image-embedder.rb
@@ -1,13 +1,28 @@
 #!/usr/bin/ruby
 
 require 'json'
+require 'net/http'
+require 'uri'
+
+$image_root = File.new('exported_content/downloaded_images')
 
 full_doc = JSON::parse(File.read(ARGV[0]))
 all_posts = full_doc['data']['posts']
 
 all_posts.each {
     |post|
+    puts "#{post['slug']}:"
     parsed_mobiledoc = JSON.parse(post['mobiledoc'])
-    p parsed_mobiledoc
+
+    image_dir = File.join($image_root, post['slug'])
+    parsed_mobiledoc['cards'].each {
+        |(type, card)|
+        next unless type == 'image'
+        filename = File.basename(card['src'])
+        cachename = File.join(image_dir, filename)
+        uri = URI(card['src'])
+
+        puts "  #{uri} -> #{cachename}"
+    }
     exit
 }
