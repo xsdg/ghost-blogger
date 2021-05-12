c4a94b69298852745978f0e21f9bd0e48fe46ae7
Omari Stephens <xsdg@xsdg.org>
2021-05-12 20:31:07 +0000

    Actually fetches referenced images and avoids re-downloading images that are current

--- image-embedder.rb
+++ image-embedder.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
+# coding: utf-8
 
+require 'fileutils'
 require 'json'
 require 'net/http'
 require 'uri'
@@ -9,12 +11,47 @@ $image_root = File.new('exported_content/downloaded_images')
 full_doc = JSON::parse(File.read(ARGV[0]))
 all_posts = full_doc['data']['posts']
 
+def cache_file_locally(uri, local_filename)
+    Net::HTTP.start(uri.host) {
+        |http|
+        if File.exists?(local_filename)
+            # Skip downloading if cached file length is as expected.
+            local_file_size = File.size(local_filename)
+
+            http.request_head(uri.path) {
+                |response|
+                canonical_length = response['Content-Length'].to_i
+                if local_file_size == canonical_length
+                    $stderr.puts "Skipping download of #{uri}; local file is complete"
+                    return
+                else
+                    $stderr.puts "Length mismatch for #{uri}: local file size #{local_file_size} vs canonical #{canonical_length}"
+                    raise "mismatch"
+                end
+            }
+        end
+
+        # Download and write to local_filename.
+        $stderr.print("Fetching #{uri}…")
+        $stderr.flush()
+        http.request_get(uri.path) {
+            |response|
+            File.open(local_filename, 'w') {
+                |local_file|
+                local_file.write(response.body())
+            }
+        }
+        $stderr.puts(" done!")
+    }
+end
+
 all_posts.each {
     |post|
     puts "#{post['slug']}:"
     parsed_mobiledoc = JSON.parse(post['mobiledoc'])
 
     image_dir = File.join($image_root, post['slug'])
+    FileUtils.makedirs(image_dir)
     parsed_mobiledoc['cards'].each {
         |(type, card)|
         next unless type == 'image'
@@ -22,7 +59,10 @@ all_posts.each {
         cachename = File.join(image_dir, filename)
         uri = URI(card['src'])
 
+        cache_file_locally(uri, cachename)
+
         puts "  #{uri} -> #{cachename}"
+        sleep(0.2)
     }
     exit
 }
